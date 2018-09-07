45ab20ae83a73e0b93349a152892017353c02b20
Hoang Trinh <hoang.trinhj@gmail.com>
2018-09-07 15:52:48 +0700

    Fix css

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,9 @@ var router = express.Router();
 const uploadToS3 = require('../helpers/s3');
 
 router.get('/upload', function (req, res, next) {
-    res.render('upload');
+    res.render('upload', {
+        title: 'Upload file'
+    });
 });
 
 router.post('/upload', uploadToS3.single('file'), (req, res, next) => {
